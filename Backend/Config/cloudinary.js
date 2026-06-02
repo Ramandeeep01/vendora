1e88665189b893f846b2a17caef2ee052b6809ce
Oscar <rmnrmn909@gmail.com>
2026-06-02 16:41:39 +0530

    Local DB to Atlas

--- Backend/Config/cloudinary.js
+++ Backend/Config/cloudinary.js
@@ -1,9 +1,5 @@
 const cloudinary = require("cloudinary").v2;
 
-console.log("CLOUDINARY CONFIG:");
-console.log("CLOUD_NAME:", process.env.CLOUD_NAME);
-console.log("API_KEY:", process.env.API_KEY);
-
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
--- Backend/connections.js
+++ Backend/connections.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function connectDB(url) {
-    return mongoose.connect(url)
+    return mongoose.connect(url);
 };
 
 module.exports = {
